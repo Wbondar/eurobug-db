c232dfb757faf0fe46206b95d1ac017528ca6b7f
Vladyslav Bondarenko <vladyslavbond@protonmail.ch>
2015-06-09 04:30:56 +0200

    Initial commit.

--- scripts/doc.sh
+++ scripts/doc.sh
@@ -1,7 +1,8 @@
 source ./conf/credentials.conf
 
-git checkout gh-pages
-java -jar schemaSpy_5.0.0.jar -t mysql -u $USER_OWNER_NAME -p $USER_OWNER_PASSWORD -db $DATABASE_SCHEMA -host $HOST:$PORT -o . -dp $CONNECTOR_J -hq -norows
+java -jar schemaSpy_5.0.0.jar -t mysql -u $USER_OWNER_NAME -p $USER_OWNER_PASSWORD -db $DATABASE_SCHEMA -host $HOST:$PORT -o /tmp/schemaSpy_5docs -dp $CONNECTOR_J -hq -norows
+git checkout --orphan gh-pages
+mv /tmp/schemaSpy_5docs/* .
 git add *
 git commit -a -m "$1"
 git push -u hub gh-pages:gh-pages
